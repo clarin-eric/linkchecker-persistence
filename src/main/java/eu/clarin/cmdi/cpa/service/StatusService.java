b586f10489c4c0346f78313791daf8c93e4df8ea
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2022-09-15 13:58:59 +0200

    adding services again

---
+++ src/main/java/eu/clarin/cmdi/cpa/service/StatusService.java
@@ -0,0 +1,43 @@
+package eu.clarin.cmdi.cpa.service;
+
+import java.time.LocalDateTime;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import eu.clarin.cmdi.cpa.model.*;
+import eu.clarin.cmdi.cpa.repository.HistoryRepository;
+import eu.clarin.cmdi.cpa.repository.StatusRepository;
+
+@Service
+@Transactional
+public class StatusService {
+   @Autowired
+   StatusRepository sRep;
+   @Autowired
+   HistoryRepository hRep;
+
+   
+   @Transactional
+   public void save(Status status) {
+      
+      sRep.findByUrl(status.getUrl()).ifPresent(oldStatus -> {//save record to history
+         History history = new History(oldStatus.getUrl(), oldStatus.getCategory(), oldStatus.getCheckingDate());
+         history.setMethod(oldStatus.getMethod());
+         history.setStatusCode(oldStatus.getStatusCode());
+         history.setMessage(oldStatus.getMessage());
+         history.setContentType(oldStatus.getContentType());
+         history.setDuration(oldStatus.getDuration());
+         history.setContentLength(oldStatus.getContentLength());
+         history.setRedirectCount(oldStatus.getRedirectCount());
+         
+         hRep.save(history);
+         
+         status.setId(oldStatus.getId());
+      }); 
+      
+      sRep.save(status); //insert or update
+   }
+}
