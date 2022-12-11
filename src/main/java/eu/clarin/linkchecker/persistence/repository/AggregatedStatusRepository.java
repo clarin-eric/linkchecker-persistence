4493541193220d5a173e76f2155f55ed97c95a09
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2022-12-11 18:03:06 +0100

    correcting type for composed Id

--- src/main/java/eu/clarin/linkchecker/persistence/repository/AggregatedStatusRepository.java
+++ src/main/java/eu/clarin/linkchecker/persistence/repository/AggregatedStatusRepository.java
@@ -5,8 +5,9 @@ import java.util.stream.Stream;
 import org.springframework.data.repository.CrudRepository;
 
 import eu.clarin.linkchecker.persistence.model.AggregatedStatus;
+import eu.clarin.linkchecker.persistence.model.AggregatedStatusId;
 
-public interface AggregatedStatusRepository extends CrudRepository<AggregatedStatus, Long> {
+public interface AggregatedStatusRepository extends CrudRepository<AggregatedStatus, AggregatedStatusId> {
    
    Stream<AggregatedStatus> findAllByProvidergroupName(String providergroupName);
    
