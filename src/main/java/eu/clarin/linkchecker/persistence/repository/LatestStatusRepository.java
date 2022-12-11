93fa451399d19bfe1ef1dae3d0b90f1f999a86da
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2022-12-11 19:40:11 +0100

    adding Stream method since findAll returns Iterable

--- src/main/java/eu/clarin/linkchecker/persistence/repository/LatestStatusRepository.java
+++ src/main/java/eu/clarin/linkchecker/persistence/repository/LatestStatusRepository.java
@@ -4,6 +4,8 @@
  */
 package eu.clarin.linkchecker.persistence.repository;
 
+import java.util.stream.Stream;
+
 import org.springframework.data.repository.CrudRepository;
 
 import eu.clarin.linkchecker.persistence.model.LatestStatus;
@@ -13,5 +15,7 @@ import eu.clarin.linkchecker.persistence.model.LatestStatusId;
  *
  */
 public interface LatestStatusRepository extends CrudRepository<LatestStatus, LatestStatusId>{
+   
+   public Stream<LatestStatus> findAllBy();
 
 }
