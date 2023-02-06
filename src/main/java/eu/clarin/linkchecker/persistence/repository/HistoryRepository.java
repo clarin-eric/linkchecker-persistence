7963325fd916de3d250a0228dacf98406c182a5e
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2023-02-06 16:49:21 +0100

    adding missing field in query

--- src/main/java/eu/clarin/linkchecker/persistence/repository/HistoryRepository.java
+++ src/main/java/eu/clarin/linkchecker/persistence/repository/HistoryRepository.java
@@ -11,20 +11,22 @@ import eu.clarin.linkchecker.persistence.model.History;
 public interface HistoryRepository extends PagingAndSortingRepository<History, Long> {
    
    @Query(
-         value = "INSERT INTO obsolete (url_name, client_name, providergroup_name, origin, expected_mime_type, ingestion_date, status_code, message, category, method, content_type, content_length, duration, checking_date, redirect_count) "
-               + "SELECT u.name, cl.name, p.name, c.origin, uc.expected_mime_type, uc.ingestion_date, h.status_code, h.message, h.category, h.method, h.content_type, h.content_length, h.duration, h.checking_date, h.redirect_count "
-               + "FROM url_context uc "
-               + "INNER JOIN (url u) "
-               + "ON u.id=uc.url_id "
-               + "INNER JOIN (context c) "
-               + "ON c.id=uc.context_id "
-               + "INNER JOIN providergroup p "
-               + "ON p.id=c.providergroup_id "
-               + "INNER JOIN history h "
-               + "ON h.url_id=u.id "
-               + "INNER JOIN client cl "
-               + "ON cl.id=c.client_id "
-               + "WHERE uc.ingestion_date < ?1",
+         value = """
+               INSERT INTO obsolete (url_name, client_name, providergroup_name, origin, expected_mime_type, ingestion_date, status_code, message, category, method, content_type, content_length, duration, checking_date, redirect_count, deletion_date) 
+               SELECT u.name, cl.name, p.name, c.origin, uc.expected_mime_type, uc.ingestion_date, h.status_code, h.message, h.category, h.method, h.content_type, h.content_length, h.duration, h.checking_date, h.redirect_count, NOW() 
+               FROM url_context uc 
+               INNER JOIN (url u) 
+               ON u.id=uc.url_id 
+               INNER JOIN (context c) 
+               ON c.id=uc.context_id 
+               INNER JOIN providergroup p 
+               ON p.id=c.providergroup_id 
+               INNER JOIN history h 
+               ON h.url_id=u.id 
+               INNER JOIN client cl 
+               ON cl.id=c.client_id 
+               WHERE uc.ingestion_date < ?1
+               """,
          nativeQuery = true
       )
    @Modifying
