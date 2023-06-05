e8c17024da2dc37eef0d490501360b2db50b2c32
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2023-06-05 16:42:23 +0200

    deleting obsolete import

--- src/main/java/eu/clarin/linkchecker/persistence/service/LinkService.java
+++ src/main/java/eu/clarin/linkchecker/persistence/service/LinkService.java
@@ -1,7 +1,6 @@
 package eu.clarin.linkchecker.persistence.service;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
