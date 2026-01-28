e4eb4f0daccc0e785abcbb956c38d6957925d853
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2026-01-28 18:47:48 +0100

    adding method findAggregatedStatus

--- src/main/java/eu/clarin/linkchecker/persistence/repository/StatusRepository.java
+++ src/main/java/eu/clarin/linkchecker/persistence/repository/StatusRepository.java
@@ -16,117 +16,117 @@ import java.util.stream.Stream;
 
 
 public interface StatusRepository extends CrudRepository<Status, Long> {
-   
-   
-   Optional<Status> findByUrl(Url url);
-   
-   Optional<Status> findByUrlName(String name);
-   
-   Stream<Status> findAllByUrlNameIn(String... names);
-   
-   Stream<Status> findAllByCategory(Category category);
-   
-   Stream<Status> findAllByUrlUrlContextsContextClientName(String name);
-   
-   Stream<Status> findAllByUrlUrlContextsContextClientNameAndUrlUrlContextsContextOrigin(String name, String origin);
-   
-   
-   @Query("SELECT s FROM Status s JOIN s.url u JOIN u.urlContexts uc JOIN uc.context c JOIN c.providergroup p ON uc.active = true AND p.name= :providergroupName AND s.category= :category")
-   Stream<Status> findAllByProvidergroupAndCategory(@Param("providergroupName") String providergroupName, @Param("category") Category category);
 
-   @Query(
-        """
-        SELECT new eu.clarin.linkchecker.persistence.model.AggregatedStatus(p.name, s.category, avg(s.duration), max(s.duration), count(u.id), sum(CASE WHEN s.duration IS NULL THEN 1 ELSE 0 END))
-                FROM Providergroup p
-                        JOIN p.contexts c
-                                JOIN c.urlContexts uc
-                                        JOIN uc.url u
-                                                JOIN u.status s
-                                                        WHERE uc.active = true
-                                                                GROUP BY p.id, s.category
-        """
-   )
-   Stream<AggregatedStatus> findAggregatedStatus();
 
-   @Query(
-        """
-            SELECT new eu.clarin.linkchecker.persistence.model.StatusDetail(
-                        p.name, 
-                        c.origin, 
-                        u.name, 
-                        s.method, 
-                        s.statusCode, 
-                        s.category, 
-                        s.message, 
-                        s.checkingDate, 
-                        s.contentType, 
-                        uc.expectedMimeType, 
-                        s.contentLength, 
-                        s.duration, 
-                        s.redirectCount
-                    )
-                    FROM Providergroup p
-                            JOIN p.contexts c
-                                    JOIN c.urlContexts uc
-                                            JOIN uc.url u
-                                                    JOIN u.status s
-                                                            WHERE uc.active = true
-                                                                    AND p.name = :providergroupName
-                                                                        AND s.category = :category
-                                                                                ORDER BY s.checkingDate DESC 
-        """
-   )
-   Stream<StatusDetail> findStatusDetail(@Param("providergroupName") String providergroupName, @Param("category") Category  category);
+    Optional<Status> findByUrl(Url url);
+
+    Optional<Status> findByUrlName(String name);
+
+    Stream<Status> findAllByUrlNameIn(String... names);
+
+    Stream<Status> findAllByCategory(Category category);
+
+    Stream<Status> findAllByUrlUrlContextsContextClientName(String name);
+
+    Stream<Status> findAllByUrlUrlContextsContextClientNameAndUrlUrlContextsContextOrigin(String name, String origin);
+
+
+    @Query("SELECT s FROM Status s JOIN s.url u JOIN u.urlContexts uc JOIN uc.context c JOIN c.providergroup p ON uc.active = true AND p.name= :providergroupName AND s.category= :category")
+    Stream<Status> findAllByProvidergroupAndCategory(@Param("providergroupName") String providergroupName, @Param("category") Category category);
 
     @Query(
             """
-                SELECT new eu.clarin.linkchecker.persistence.model.StatusDetail(
-                            p.name,
-                            c.origin,
-                            u.name,
-                            s.method,
-                            s.statusCode,
-                            s.category,
-                            s.message,
-                            s.checkingDate,
-                            s.contentType,
-                            uc.expectedMimeType,
-                            s.contentLength,
-                            s.duration,
-                            s.redirectCount
-                        )
-                        FROM Providergroup p
-                                JOIN p.contexts c
-                                        JOIN c.urlContexts uc
-                                                JOIN uc.url u
-                                                        JOIN u.status s
+                    SELECT new eu.clarin.linkchecker.persistence.model.AggregatedStatus(p.name, s.category, avg(s.duration), max(s.duration), count(u), count(s.duration))
+                            FROM Providergroup p
+                                    JOIN p.contexts c
+                                            JOIN c.urlContexts uc
+                                                    JOIN uc.url u
+                                                            JOIN u.status s
                                                                     WHERE uc.active = true
-                                                                            AND s.category = :category
-                                                                                    ORDER BY s.checkingDate DESC
-            """
+                                                                            GROUP BY p.id, s.category
+                    """
     )
-    Stream<StatusDetail> findStatusDetail(@Param("category") Category  category);
-   
-   @Query(
-         value = """
-                INSERT INTO obsolete (url_name, client_name, providergroup_name, origin, expected_mime_type, ingestion_date, status_code, message, category, method, content_type, content_length, duration, checking_date, redirect_count, deletion_date) 
-                SELECT u.name, cl.name, p.name, c.origin, uc.expected_mime_type, uc.ingestion_date, s.status_code, s.message, s.category, s.method, s.content_type, s.content_length, s.duration, s.checking_date, s.redirect_count, NOW() 
-                FROM url_context uc 
-                INNER JOIN (url u) 
-                ON u.id=uc.url_id 
-                INNER JOIN (context c) 
-                ON c.id=uc.context_id 
-                INNER JOIN providergroup p 
-                ON p.id=c.providergroup_id 
-                INNER JOIN status s 
-                ON s.url_id=u.id 
-                INNER JOIN client cl 
-                ON cl.id=c.client_id 
-                WHERE uc.ingestion_date < ?1
-               """, 
-         nativeQuery = true
-      )
-   @Modifying
-   void saveStatusLinksOlderThan(LocalDateTime dateTime);
+    Stream<AggregatedStatus> findAggregatedStatus();
+
+    @Query(
+            """
+                        SELECT new eu.clarin.linkchecker.persistence.model.StatusDetail(
+                                    p.name, 
+                                    c.origin, 
+                                    u.name, 
+                                    s.method, 
+                                    s.statusCode, 
+                                    s.category, 
+                                    s.message, 
+                                    s.checkingDate, 
+                                    s.contentType, 
+                                    uc.expectedMimeType, 
+                                    s.contentLength, 
+                                    s.duration, 
+                                    s.redirectCount
+                                )
+                                FROM Providergroup p
+                                        JOIN p.contexts c
+                                                JOIN c.urlContexts uc
+                                                        JOIN uc.url u
+                                                                JOIN u.status s
+                                                                        WHERE uc.active = true
+                                                                                AND p.name = :providergroupName
+                                                                                    AND s.category = :category
+                                                                                            ORDER BY s.checkingDate DESC 
+                    """
+    )
+    Stream<StatusDetail> findStatusDetail(@Param("providergroupName") String providergroupName, @Param("category") Category category);
+
+    @Query(
+            """
+                        SELECT new eu.clarin.linkchecker.persistence.model.StatusDetail(
+                                    p.name,
+                                    c.origin,
+                                    u.name,
+                                    s.method,
+                                    s.statusCode,
+                                    s.category,
+                                    s.message,
+                                    s.checkingDate,
+                                    s.contentType,
+                                    uc.expectedMimeType,
+                                    s.contentLength,
+                                    s.duration,
+                                    s.redirectCount
+                                )
+                                FROM Providergroup p
+                                        JOIN p.contexts c
+                                                JOIN c.urlContexts uc
+                                                        JOIN uc.url u
+                                                                JOIN u.status s
+                                                                            WHERE uc.active = true
+                                                                                    AND s.category = :category
+                                                                                            ORDER BY s.checkingDate DESC
+                    """
+    )
+    Stream<StatusDetail> findStatusDetail(@Param("category") Category category);
+
+    @Query(
+            value = """
+                     INSERT INTO obsolete (url_name, client_name, providergroup_name, origin, expected_mime_type, ingestion_date, status_code, message, category, method, content_type, content_length, duration, checking_date, redirect_count, deletion_date) 
+                     SELECT u.name, cl.name, p.name, c.origin, uc.expected_mime_type, uc.ingestion_date, s.status_code, s.message, s.category, s.method, s.content_type, s.content_length, s.duration, s.checking_date, s.redirect_count, NOW() 
+                     FROM url_context uc 
+                     INNER JOIN (url u) 
+                     ON u.id=uc.url_id 
+                     INNER JOIN (context c) 
+                     ON c.id=uc.context_id 
+                     INNER JOIN providergroup p 
+                     ON p.id=c.providergroup_id 
+                     INNER JOIN status s 
+                     ON s.url_id=u.id 
+                     INNER JOIN client cl 
+                     ON cl.id=c.client_id 
+                     WHERE uc.ingestion_date < :dateTime
+                    """,
+            nativeQuery = true
+    )
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    void saveStatusLinksOlderThan(@Param("dateTime") LocalDateTime dateTime);
 
 }
--- src/test/java/eu/clarin/linkchecker/persistence/repositories/StatusRepositoryTests.java
+++ src/test/java/eu/clarin/linkchecker/persistence/repositories/StatusRepositoryTests.java
@@ -1,16 +1,11 @@
 package eu.clarin.linkchecker.persistence.repositories;
 
+import eu.clarin.linkchecker.persistence.model.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataAccessException;
 import org.springframework.transaction.annotation.Transactional;
 
-import eu.clarin.linkchecker.persistence.model.Client;
-import eu.clarin.linkchecker.persistence.model.Context;
-import eu.clarin.linkchecker.persistence.model.Role;
-import eu.clarin.linkchecker.persistence.model.Status;
-import eu.clarin.linkchecker.persistence.model.Url;
-import eu.clarin.linkchecker.persistence.model.UrlContext;
 import eu.clarin.linkchecker.persistence.utils.Category;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -163,5 +158,77 @@ class StatusRepositoryTests extends RepositoryTests {
         });
 
         Stream.of(contexts).forEach(context -> assertEquals(1, sRep.findAllByUrlUrlContextsContextClientNameAndUrlUrlContextsContextOrigin(context.getClient().getName(), context.getOrigin()).count()));
+    }
+
+    @Test
+    @Transactional
+    void findAggregatedStatus(){
+
+        Client client = usRep.save(new Client("wowasa1", "pw", Role.USER));
+
+        Providergroup pg1 = pRep.save(new Providergroup("pg1"));
+        Providergroup pg2 = pRep.save(new Providergroup("pg2"));
+
+        Context c1 = cRep.save(new Context("file", pg1, client));
+        Context c2 = cRep.save(new Context("file", pg2, client));
+
+        // 10 urls for pg1, Category.Ok, all with duration
+        IntStream.range(0, 10).forEach(i -> {
+            Url url = uRep.save(new Url("http://www.wowasa.com/page" + i, "www.wowasa.com", true));
+
+            ucRep.save(new UrlContext(url, c1, LocalDateTime.now(), true));
+
+            Status status = new Status(url, Category.Ok, "", LocalDateTime.now());
+            status.setDuration(1 + i);
+
+            sRep.save(status);
+        });
+        // 10 urls for pg1, Category.Undetermined, only half with duration
+        IntStream.range(10, 20).forEach(i -> {
+            Url url = uRep.save(new Url("http://www.wowasa.com/page" + i, "www.wowasa.com", true));
+
+            ucRep.save(new UrlContext(url, c1, LocalDateTime.now(), true));
+
+            Status status = new Status(url, Category.Undetermined, "", LocalDateTime.now());
+            if(i%2 == 0) {
+                status.setDuration(1 + i);
+            }
+
+            sRep.save(status);
+        });
+        // 10 non-active urls for pg1, Category.Ok
+        IntStream.range(20, 30).forEach(i -> {
+            Url url = uRep.save(new Url("http://www.wowasa.com/page" + i, "www.wowasa.com", true));
+
+            ucRep.save(new UrlContext(url, c2, LocalDateTime.now(), false));
+
+            sRep.save(new Status(url, Category.Ok, "", LocalDateTime.now()));
+        });
+        // 10 urls for pg1, Category.Blocked_By_Robots_txt
+        IntStream.range(30, 40).forEach(i -> {
+            Url url = uRep.save(new Url("http://www.wowasa.com/page" + i, "www.wowasa.com", true));
+
+            ucRep.save(new UrlContext(url, c2, LocalDateTime.now(), true));
+
+            sRep.save(new Status(url, Category.Blocked_By_Robots_txt, "", LocalDateTime.now()));
+        });
+        // only three lines, since the non-active urls from pg2 are ignored
+        assertEquals(3, sRep.findAggregatedStatus().count());
+
+        sRep.findAggregatedStatus()
+                .filter(aggregatedStatus -> aggregatedStatus.getCategory().equals(Category.Ok) && aggregatedStatus.getProvidergroupName().equals("pg1"))
+                    .findFirst().ifPresent(aggregatedStatus -> {
+                        assertEquals(10, aggregatedStatus.getMaxDuration());
+                        assertEquals(5.5, aggregatedStatus.getAvgDuration());
+                        assertEquals(10, aggregatedStatus.getNumberId());
+                        assertEquals(10, aggregatedStatus.getNumberDuration());
+                });
+        // only 5 with duration
+        sRep.findAggregatedStatus()
+                .filter(aggregatedStatus -> aggregatedStatus.getCategory().equals(Category.Undetermined) && aggregatedStatus.getProvidergroupName().equals("pg1"))
+                .findFirst().ifPresent(aggregatedStatus -> {
+                    assertEquals(10, aggregatedStatus.getNumberId());
+                    assertEquals(5, aggregatedStatus.getNumberDuration());
+                });
     }
 }
