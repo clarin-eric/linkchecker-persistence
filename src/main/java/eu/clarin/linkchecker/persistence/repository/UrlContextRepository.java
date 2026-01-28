0d16d979f54b6442cedd1b9ea798033fa775740e
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2026-01-28 17:32:37 +0100

    adding method updateIngestionDate

--- src/main/java/eu/clarin/linkchecker/persistence/repository/UrlContextRepository.java
+++ src/main/java/eu/clarin/linkchecker/persistence/repository/UrlContextRepository.java
@@ -13,26 +13,36 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface UrlContextRepository extends CrudRepository<UrlContext, Long> {
-   
-   Optional<UrlContext> findByUrlAndContextAndExpectedMimeType(Url url, Context context, String expectedMimeType);
-   @Modifying()
-   @Transactional()
-   @Query(
-         value = """
-               INSERT INTO url_context(url_id, context_id, expected_mime_type, active, ingestion_date)
-               VALUES(:urlId, :contextId, :expectedMimeType, TRUE, :ingestionDate)
-               ON DUPLICATE KEY UPDATE active=TRUE, ingestion_date=:ingestionDate
-            """,
-         nativeQuery = true
-      )
-   void insertOrUpdate(@Param("urlId") Long urlId, @Param("contextId") Long contextId, @Param("expectedMimeType") String expectedMimeType, @Param("ingestionDate") LocalDateTime ingestionDate);
-   
-   @Modifying(clearAutomatically = true, flushAutomatically = true)
-   @Query("UPDATE UrlContext uc SET uc.active = false WHERE uc.active = true AND uc.ingestionDate < :ingestionDate")
-   void deactivateOlderThan(@Param("ingestionDate") LocalDateTime ingestionDate);
-   
-   @Modifying(clearAutomatically = true, flushAutomatically = true)
-   @Query("DELETE FROM UrlContext uc WHERE uc.ingestionDate < :ingestionDate")
-   void deleteByIngestionDateBefore(@Param("ingestionDate") LocalDateTime ingestionDate);
 
+    Optional<UrlContext> findByUrlAndContextAndExpectedMimeType(Url url, Context context, String expectedMimeType);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Transactional()
+    @Query(
+            value = """
+                       INSERT INTO url_context(url_id, context_id, expected_mime_type, active, ingestion_date)
+                       VALUES(:urlId, :contextId, :expectedMimeType, TRUE, :ingestionDate)
+                       ON DUPLICATE KEY UPDATE active=TRUE, ingestion_date=:ingestionDate
+                    """,
+            nativeQuery = true
+    )
+    void insertOrUpdate(@Param("urlId") Long urlId, @Param("contextId") Long contextId, @Param("expectedMimeType") String expectedMimeType, @Param("ingestionDate") LocalDateTime ingestionDate);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("UPDATE UrlContext uc SET uc.active = false WHERE uc.active = true AND uc.ingestionDate < :ingestionDate")
+    void deactivateOlderThan(@Param("ingestionDate") LocalDateTime ingestionDate);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("DELETE FROM UrlContext uc WHERE uc.ingestionDate < :ingestionDate")
+    void deleteByIngestionDateBefore(@Param("ingestionDate") LocalDateTime ingestionDate);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("""
+            UPDATE UrlContext uc SET uc.ingestionDate = LOCAL_DATETIME
+                    WHERE uc.active = TRUE
+                            AND uc.context IN
+                                    (SELECT c FROM Context c JOIN c.providergroup p
+                                            WHERE p.name = :providergroupName)
+            """)
+    void updateIngestionDate(@Param("providergroupName") String providergroupName);
 }
--- src/test/java/eu/clarin/linkchecker/persistence/repositories/UrlContextRepositoryTests.java
+++ src/test/java/eu/clarin/linkchecker/persistence/repositories/UrlContextRepositoryTests.java
@@ -1,93 +1,131 @@
 package eu.clarin.linkchecker.persistence.repositories;
 
+import eu.clarin.linkchecker.persistence.model.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-
-import eu.clarin.linkchecker.persistence.model.Client;
-import eu.clarin.linkchecker.persistence.model.Context;
-import eu.clarin.linkchecker.persistence.model.Role;
-import eu.clarin.linkchecker.persistence.model.Url;
-import eu.clarin.linkchecker.persistence.model.UrlContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDateTime;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 @SpringBootTest
-class UrlContextRepositoryTests extends RepositoryTests{
+class UrlContextRepositoryTests extends RepositoryTests {
 
-   @Test
-   void save() {
+    @Test
+    void save() {
 
-      Url url = uRep.save(new Url("http://www.wowasa.com", "www.wowasa.com", true));
+        Url url = uRep.save(new Url("http://www.wowasa.com", "www.wowasa.com", true));
 
-      Client client = usRep.save(new Client("wowasa", "xxxxxxxxxxxxxxxx", Role.ADMIN));
+        Client client = usRep.save(new Client("wowasa", "xxxxxxxxxxxxxxxx", Role.ADMIN));
 
-      Context context = cRep.save(new Context("upload" + System.currentTimeMillis(), null, client));
-      
-      UrlContext urlContext = new UrlContext(url, context, LocalDateTime.now(), true);
-      urlContext.setActive(true);
+        Context context = cRep.save(new Context("upload" + System.currentTimeMillis(), null, client));
 
-      ucRep.save(urlContext);
+        UrlContext urlContext = new UrlContext(url, context, LocalDateTime.now(), true);
+        urlContext.setActive(true);
 
-      assertEquals(1, ucRep.count());
+        ucRep.save(urlContext);
 
-   }
+        assertEquals(1, ucRep.count());
 
-   @Test
-   @Transactional
-   void deleteOlderThan() {
-      
-      LocalDateTime now = LocalDateTime.now();
+    }
 
-      Url url = uRep.save(new Url("http://www.wowasa.com", "www.wowasa.com", true));
+    @Test
+    @Transactional
+    void deleteOlderThan() {
 
-      Client client = usRep.save(new Client("wowasa", "xxxxxxxxxxxxxxxx", Role.ADMIN));
+        LocalDateTime now = LocalDateTime.now();
 
-      Context context = cRep.save(new Context("upload" + System.currentTimeMillis(), null, client));
-      
-      IntStream.range(0, 100)
-         .forEach(i -> {
-            
-            UrlContext urlContext = new UrlContext(url, context, now.minusDays(i), false);
-            urlContext.setActive(true);
-            urlContext.setExpectedMimeType("mimetype-" + i);
+        Url url = uRep.save(new Url("http://www.wowasa.com", "www.wowasa.com", true));
 
-            ucRep.save(urlContext);           
-         });
+        Client client = usRep.save(new Client("wowasa", "xxxxxxxxxxxxxxxx", Role.ADMIN));
 
-      ucRep.deleteByIngestionDateBefore(now.minusDays(50));
+        Context context = cRep.save(new Context("upload" + System.currentTimeMillis(), null, client));
 
-      assertEquals(50, ucRep.count());
-   }
+        IntStream.range(0, 100)
+                .forEach(i -> {
+                    UrlContext urlContext = new UrlContext(url, context, now.minusDays(i).minusHours(1), true);
+                    urlContext.setExpectedMimeType("mimetype-" + i);
 
-   @Test
-   @Transactional
-   void deactivateOlderThan() {
-      
-      LocalDateTime now = LocalDateTime.now();
+                    ucRep.save(urlContext);
+                });
 
-      Url url = uRep.save(new Url("http://www.wowasa.com", "www.wowasa.com", true));
+        ucRep.deleteByIngestionDateBefore(now.minusDays(50));
 
-      Client client = usRep.save(new Client("wowasa", "xxxxxxxxxxxxxxxx", Role.ADMIN));
+        assertEquals(50, ucRep.count());
+    }
 
-      Context context = cRep.save(new Context("upload" + System.currentTimeMillis(), null, client));
-      
-      IntStream.range(0, 100)
-      .forEach(i -> {
-         
-         UrlContext urlContext = new UrlContext(url, context, now.minusDays(i), false);
-         urlContext.setActive(true);
-         urlContext.setExpectedMimeType("mimetype-" + i);
+    @Test
+    @Transactional
+    void deactivateOlderThan() {
 
-         ucRep.save(urlContext);           
-      });
+        LocalDateTime now = LocalDateTime.now();
 
-      ucRep.deactivateOlderThan(now.minusDays(50));
+        Url url = uRep.save(new Url("http://www.wowasa.com", "www.wowasa.com", true));
 
-      assertEquals(50, StreamSupport.stream(ucRep.findAll().spliterator(), false).filter(UrlContext::getActive).count());
-   }
+        Client client = usRep.save(new Client("wowasa", "xxxxxxxxxxxxxxxx", Role.ADMIN));
+
+        Context context = cRep.save(new Context("upload" + System.currentTimeMillis(), null, client));
+
+        IntStream.range(0, 100)
+                .forEach(i -> {
+
+                    UrlContext urlContext = new UrlContext(url, context, now.minusDays(i).minusHours(1), true);
+                    urlContext.setExpectedMimeType("mimetype-" + i);
+
+                    ucRep.save(urlContext);
+                });
+
+        ucRep.deactivateOlderThan(now.minusDays(50));
+
+        assertEquals(50, StreamSupport.stream(ucRep.findAll().spliterator(), false).filter(UrlContext::getActive).count());
+    }
+
+    @Test
+    @Transactional
+    void updateIngestionDate() {
+        LocalDateTime fixedDateTime = LocalDateTime.now().minusHours(1);
+
+        Url url = uRep.save(new Url("http://www.wowasa.com", "www.wowasa.com", true));
+
+        Client client = usRep.save(new Client("wowasa", "xxxxxxxxxxxxxxxx", Role.ADMIN));
+
+        Providergroup providergroup1 = pRep.save(new Providergroup("pg1"));
+        Providergroup providergroup2 = pRep.save(new Providergroup("pg2"));
+
+        Context context1 = cRep.save(new Context("c1", providergroup1, client));
+        Context context2 = cRep.save(new Context("c2", providergroup2, client));
+
+        // creating 20 url-contexts for context1, therefrom 10 active and 10 non-active
+        IntStream.range(0, 20)
+                .forEach(i -> {
+
+                    UrlContext urlContext = new UrlContext(url, context1, fixedDateTime.minusDays(1), i%2 == 0);
+                    urlContext.setExpectedMimeType("mimetype-" + i);
+
+                    ucRep.save(urlContext);
+                });
+        // creating 20 url-contexts for context2, therefrom 10 active and 10 non-active
+        IntStream.range(0, 20)
+                .forEach(i -> {
+
+                    UrlContext urlContext = new UrlContext(url, context2, fixedDateTime.minusDays(1), i%2 == 0);
+                    urlContext.setExpectedMimeType("mimetype-" + i);
+
+                    ucRep.save(urlContext);
+                });
+        // altogether 40 inserted
+        assertEquals(40, ucRep.count());
+        // since inserted with ingestion date (fixedDateTime - 1 day), all 40 have an ingestion date before fixedDateTime
+        assertEquals(40, StreamSupport.stream(ucRep.findAll().spliterator(), false).filter(uc -> uc.getIngestionDate().isBefore(fixedDateTime)).count());
+        // we update the ingestionDate to now(), which is after fixedDateTime
+        ucRep.updateIngestionDate("pg1");
+        // 30 entries remain unchanged
+        assertEquals(30, StreamSupport.stream(ucRep.findAll().spliterator(), false).filter(uc -> uc.getIngestionDate().isBefore(fixedDateTime)).count());
+        // but for the 10 active url-contexts of providergroup1 (with context1) we set ingestionDate to now() > fixedDateTime
+        assertEquals(10, StreamSupport.stream(ucRep.findAll().spliterator(), false).filter(uc -> uc.getIngestionDate().isAfter(fixedDateTime)).count());
+    }
 }
