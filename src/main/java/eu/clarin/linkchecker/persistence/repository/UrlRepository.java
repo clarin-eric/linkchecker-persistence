114b420adcc55be84be82b79d0974aa6605766fa
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2026-01-28 19:08:16 +0100

    adding method aggregateCountUrl

--- src/main/java/eu/clarin/linkchecker/persistence/repository/UrlRepository.java
+++ src/main/java/eu/clarin/linkchecker/persistence/repository/UrlRepository.java
@@ -13,48 +13,49 @@ import java.util.stream.Stream;
 
 @Repository
 public interface UrlRepository extends CrudRepository<Url, Long> {
-   
-   Optional<Url> findByName(String name);
-   
-   @Modifying(clearAutomatically = true, flushAutomatically = true)
-   @Query("DELETE FROM Url u WHERE u NOT IN (SELECT DISTINCT uc.url FROM UrlContext uc)")
-   void deleteByUrlContextsIsEmpty();
-   
-   @Query(
-         value = """
-            SELECT id, name, group_key, valid, priority FROM 
-               (SELECT ROW_NUMBER() OVER (PARTITION BY u.group_key ORDER BY u.priority DESC, s.checking_date) AS order_Nr, u.id, u.name, u.group_key, u.valid, u.priority, s.checking_date 
-               FROM url u 
-               LEFT JOIN status s ON s.url_id = u.id 
-               WHERE u.valid=true 
-               AND u.id IN (SELECT uc.url_id FROM url_context uc WHERE uc.active = true) 
-               AND (s.checking_date IS NULL OR s.checking_date < ?2)
-               ORDER BY u.group_key, u.priority DESC, s.checking_date) tab1
-            WHERE order_nr <= ?1 
-            ORDER by tab1.priority DESC, tab1.checking_date    
-            """,
-          nativeQuery = true     
-      )
-   Stream<Url> getNextUrlsToCheck(int groupLimit, LocalDateTime maximalCheckingDate);
-   
-   long countByUrlContextsActive(boolean active);
-   
-   @Query("SELECT COUNT(*) FROM Url u JOIN u.urlContexts uc JOIN uc.context c JOIN c.providergroup p ON p.name=?1")
-   long countByProvidergroupName(String providergroupName);
-   
-   @Query("SELECT DISTINCT COUNT(u.id) FROM Url u JOIN u.urlContexts uc JOIN uc.context c JOIN c.providergroup p ON p.name=?1")
-   long countDistinctByProvidergroupName(String providergroupName);
 
-   @Query(
+    Optional<Url> findByName(String name);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("DELETE FROM Url u WHERE u NOT IN (SELECT DISTINCT uc.url FROM UrlContext uc)")
+    void deleteByUrlContextsIsEmpty();
+
+    @Query(
+            value = """
+                    SELECT id, name, group_key, valid, priority FROM 
+                       (SELECT ROW_NUMBER() OVER (PARTITION BY u.group_key ORDER BY u.priority DESC, s.checking_date) AS order_Nr, u.id, u.name, u.group_key, u.valid, u.priority, s.checking_date 
+                       FROM url u 
+                       LEFT JOIN status s ON s.url_id = u.id 
+                       WHERE u.valid=true 
+                       AND u.id IN (SELECT uc.url_id FROM url_context uc WHERE uc.active = true) 
+                       AND (s.checking_date IS NULL OR s.checking_date < ?2)
+                       ORDER BY u.group_key, u.priority DESC, s.checking_date) tab1
+                    WHERE order_nr <= ?1 
+                    ORDER by tab1.priority DESC, tab1.checking_date    
+                    """,
+            nativeQuery = true
+    )
+    Stream<Url> getNextUrlsToCheck(int groupLimit, LocalDateTime maximalCheckingDate);
+
+    long countByUrlContextsActive(boolean active);
+
+    @Query("SELECT COUNT(*) FROM Url u JOIN u.urlContexts uc JOIN uc.context c JOIN c.providergroup p ON p.name=?1")
+    long countByProvidergroupName(String providergroupName);
+
+    @Query("SELECT DISTINCT COUNT(u.id) FROM Url u JOIN u.urlContexts uc JOIN uc.context c JOIN c.providergroup p ON p.name=?1")
+    long countDistinctByProvidergroupName(String providergroupName);
+
+    @Query(
             """
-            SELECT new eu.clarin.linkchecker.persistence.model.UrlCount(p.name, COUNT(u.id), COUNT(DISTINCT u.id))
-                        FROM Url u
+                    SELECT new eu.clarin.linkchecker.persistence.model.UrlCount(p.name, COUNT(u), COUNT(DISTINCT u))
+                            FROM Url u
                                     JOIN u.urlContexts uc
-                                                JOIN uc.context c
-                                                            JOIN c.providergroup p
-                                                                        GROUP BY p.id           
-            """
-   )
-   Stream<UrlCount> aggregateCountUrl();
+                                            JOIN uc.context c
+                                                    JOIN c.providergroup p
+                                                            WHERE uc.active = true
+                                                                    GROUP BY p.id           
+                    """
+    )
+    Stream<UrlCount> aggregateCountUrl();
 
 }
