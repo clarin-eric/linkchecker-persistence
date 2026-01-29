c3b67ee04d767556fffcfe645b011e5cfe34e7c5
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2026-01-29 19:13:34 +0100

    formatting

--- src/main/java/eu/clarin/linkchecker/persistence/repository/UrlRepository.java
+++ src/main/java/eu/clarin/linkchecker/persistence/repository/UrlRepository.java
@@ -5,6 +5,7 @@ import eu.clarin.linkchecker.persistence.model.UrlCount;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -22,16 +23,16 @@ public interface UrlRepository extends CrudRepository<Url, Long> {
 
     @Query(
             value = """
-                    SELECT id, name, group_key, valid, priority FROM 
-                       (SELECT ROW_NUMBER() OVER (PARTITION BY u.group_key ORDER BY u.priority DESC, s.checking_date) AS order_Nr, u.id, u.name, u.group_key, u.valid, u.priority, s.checking_date 
-                       FROM url u 
-                       LEFT JOIN status s ON s.url_id = u.id 
-                       WHERE u.valid=true 
-                       AND u.id IN (SELECT uc.url_id FROM url_context uc WHERE uc.active = true) 
+                    SELECT id, name, group_key, valid, priority FROM
+                       (SELECT ROW_NUMBER() OVER (PARTITION BY u.group_key ORDER BY u.priority DESC, s.checking_date) AS order_Nr, u.id, u.name, u.group_key, u.valid, u.priority, s.checking_date
+                       FROM url u
+                       LEFT JOIN status s ON s.url_id = u.id
+                       WHERE u.valid=true
+                       AND u.id IN (SELECT uc.url_id FROM url_context uc WHERE uc.active = true)
                        AND (s.checking_date IS NULL OR s.checking_date < ?2)
                        ORDER BY u.group_key, u.priority DESC, s.checking_date) tab1
-                    WHERE order_nr <= ?1 
-                    ORDER by tab1.priority DESC, tab1.checking_date    
+                    WHERE order_nr <= ?1
+                    ORDER by tab1.priority DESC, tab1.checking_date
                     """,
             nativeQuery = true
     )
@@ -39,11 +40,11 @@ public interface UrlRepository extends CrudRepository<Url, Long> {
 
     long countByUrlContextsActive(boolean active);
 
-    @Query("SELECT COUNT(*) FROM Url u JOIN u.urlContexts uc JOIN uc.context c JOIN c.providergroup p ON p.name=?1")
-    long countByProvidergroupName(String providergroupName);
+    @Query("SELECT COUNT(*) FROM Url u JOIN u.urlContexts uc JOIN uc.context c JOIN c.providergroup p ON p.name=:providergroupName")
+    long countByProvidergroupName(@Param("providergroupName") String providergroupName);
 
-    @Query("SELECT DISTINCT COUNT(u.id) FROM Url u JOIN u.urlContexts uc JOIN uc.context c JOIN c.providergroup p ON p.name=?1")
-    long countDistinctByProvidergroupName(String providergroupName);
+    @Query("SELECT DISTINCT COUNT(u.id) FROM Url u JOIN u.urlContexts uc JOIN uc.context c JOIN c.providergroup p ON p.name=:providergroupName")
+    long countDistinctByProvidergroupName(@Param("providergroupName") String providergroupName);
 
     @Query(
             """
@@ -53,7 +54,7 @@ public interface UrlRepository extends CrudRepository<Url, Long> {
                                             JOIN uc.context c
                                                     JOIN c.providergroup p
                                                             WHERE uc.active = true
-                                                                    GROUP BY p.id           
+                                                                    GROUP BY p.id
                     """
     )
     Stream<UrlCount> aggregateCountUrl();
