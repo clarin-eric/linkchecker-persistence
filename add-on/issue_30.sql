34b3267ce4cac941daffb80ca024f61872dbfaf0
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2026-08-16 11:12:18 +0200

    adding reindexing of url_context.id

--- add-on/issue_30.sql
+++ add-on/issue_30.sql
@@ -1,3 +1,6 @@
 ALTER TABLE `status` ADD COLUMN `final_url` VARCHAR(512) DEFAULT NULL;
 ALTER TABLE `history` ADD COLUMN `final_url` VARCHAR(512) DEFAULT NULL;
 ALTER TABLE `url` ADD COLUMN `exclude_checking` BOOLEAN DEFAULT NULL;
+# reindexing of the id field since the numbers became extremly high
+ALTER TABLE `url_context` DROP COLUMN `id`;
+ALTER TABLE `url_context` ADD COLUMN `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST;
