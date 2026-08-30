b2c5d85ae8e5330dd98140d918c83a61d80644b3
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2026-08-30 18:14:28 +0200

    adding view for links to check

--- add-on/issue_30.sql
+++ add-on/issue_30.sql
@@ -3,12 +3,14 @@ ALTER TABLE `history` ADD COLUMN `final_url` VARCHAR(512) DEFAULT NULL;
 ALTER TABLE `url` ADD COLUMN `exclude_checking` BOOLEAN DEFAULT NULL;
 # corrections
 # dropping table providerGroup (!= providergroup)
-DROP TABLE `providerGroup`;
+DROP TABLE IF EXISTS `providerGroup`;
 # dropping table urlToCheck
-DROP TABLE `urlToCheck`;
+DROP TABLE IF EXISTS `urlToCheck`;
 # using handle prefix instead of host as group key for handles
 UPDATE `url` SET `group_key` = SUBSTRING_INDEX(SUBSTRING_INDEX(name , '/', 4), '/', -1) WHERE `group_key` = 'hdl.handle.net';
-
+# exclude 'archive.mpi.nl' temporarely from checking to process more handles
+UPDATE `url` SET `exclude_checking`=TRUE WHERE `group_key` = 'archive.mpi.nl';
+# ordered list of URLs to check
 CREATE OR REPLACE VIEW `url_to_check` AS
     SELECT name, url_id, status_id, priority FROM
         (SELECT ROW_NUMBER() OVER (PARTITION BY u.group_key ORDER BY u.priority DESC, s.checking_date) AS order_nr, u.id AS url_id, u.name, u.group_key, u.valid, u.priority, s.id AS status_id, s.checking_date
